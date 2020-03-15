c7a9f97ae14086e573082ae6ee217ac7e195ec9b
Tom Harrop <twharrop@gmail.com>
2020-03-16 10:29:43 +1300

    filter with bcftools, stats with vcftools

--- src/generate_whitelist.R
+++ src/generate_whitelist.R
@@ -4,9 +4,7 @@ log <- file(snakemake@log[[1]], open = "wt")
 sink(log, type = "message")
 sink(log, append = TRUE, type = "output")
 
-library(adegenet)
 library(data.table)
-
 
 ###########
 # GLOBALS #
