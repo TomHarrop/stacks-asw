338c4f7af5d1daf4426a73fb6346d88668329688
Tom Harrop <twharrop@gmail.com>
2020-03-20 13:32:42 +1300

    pop p-vals

--- src/inactive/explore_phistats.R
+++ src/inactive/explore_phistats.R
@@ -5,11 +5,58 @@ fai_file <- "output/map_to_genome/draft_genome.fasta.fai"
 phistats_file <- "output/popgen/mapped/stacks_populations/populations.phistats.tsv"
 
 phistats <- fread(phistats_file, skip = 11)
-setkey(phistats, Chr, BP)
+
+fai_names <- c("Chr", "chr_length")
+
+fai <- fread(fai_file, select = 1:2, col.names = fai_names)
+
+n_to_label <- 20
+
+# chromosome coordinates
+chr_coords <- copy(fai)
+setorder(chr_coords, -chr_length, Chr)
+chr_coords[, chr_end := cumsum(chr_length)]
+chr_coords[, chr_start := chr_end - chr_length + 1]
+chr_coords[, lab_pos := chr_start + round(mean(c(chr_start, chr_end)), 0), by = Chr]
+pos_to_label = chr_coords[, seq(1, max(chr_end), length.out = n_to_label)]
+
+label_positions <- sapply(pos_to_label, function(x)
+  chr_coords[, .I[which.min(abs(x - lab_pos))]])
+
+chr_coords[label_positions, x_lab := Chr]
+chr_coords[is.na(x_lab), x_lab := ""]
+
+# homemade manhattan plot
+phistats_with_len <- merge(phistats, chr_coords, by = "Chr")
+setorder(phistats_with_len, -chr_length, Chr, BP)
+phistats_with_len[, bp_coord := BP + chr_start - 1]
+
+q99 <- phistats_with_len[, quantile(`Smoothed Phi_st`, 0.99)]
+
+ggplot(phistats_with_len, aes(x = bp_coord, y = `Smoothed Phi_st`)) +
+  theme(axis.text.x = element_text(angle = 30,
+                                   hjust = 1,
+                                   vjust = 1),
+        axis.ticks.x = element_blank(),
+        axis.ticks.length.x = unit(0, "mm")) +
+  scale_x_continuous(breaks = chr_coords[, lab_pos],
+                     labels = chr_coords[, x_lab]) +
+  geom_hline(yintercept = q99) +
+  geom_point()
 
 
-fai <- fread(fai_file)
-setorder(fai, -V2)
+d99 <- phistats_with_len[, quantile(`Smoothed D_est`, 0.99)]
+ggplot(phistats_with_len, aes(x = bp_coord, y = `Smoothed D_est`)) +
+  theme(axis.text.x = element_text(angle = 30,
+                                   hjust = 1,
+                                   vjust = 1),
+        axis.ticks.x = element_blank(),
+        axis.ticks.length.x = unit(0, "mm")) +
+  scale_x_continuous(breaks = chr_coords[, lab_pos],
+                     labels = chr_coords[, x_lab]) +
+  geom_hline(yintercept = d99) +
+  geom_point()
+
 
 plot_contig <- fai[3, V1]
 x_lim <- fai[V1 == plot_contig, c(0, V2)]
