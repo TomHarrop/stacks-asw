de643236e91367600a49a478e48b5f1252d1da56
Tom Harrop <twharrop@gmail.com>
2020-04-20 11:41:03 +1200

    refactor lookups

--- src/combine_individual_stats.R
+++ src/combine_individual_stats.R
@@ -1,9 +1,9 @@
 #!/usr/bin/env Rscript
 
 # set log
-log <- file(snakemake@log[[1]], open = "wt")
-sink(log, type = "message")
-sink(log, append = TRUE, type = "output")
+# log <- file(snakemake@log[[1]], open = "wt")
+# sink(log, type = "message")
+# sink(log, append = TRUE, type = "output")
 
 library(data.table)
 
