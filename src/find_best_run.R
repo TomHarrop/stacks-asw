1e86afcca81fd6813654868cc10fc129318c26d7
Marissa Le Lec <marissalelec@gmail.com>
2020-05-22 12:06:43 +1200

    added aic, tidied

--- src/find_best_run.R
+++ src/find_best_run.R
@@ -18,11 +18,13 @@ sink(log, append = TRUE, type = "output")
 
 
 
-# Read in output files and summarise all runs
-runs_summary <- purrr::map_dfr(bestlhoods, read_delim, delim = "\t") %>% 
-				  select(MaxEstLhood, MaxObsLhood) %>%
-  				  mutate(Lhood = MaxObsLhood - MaxEstLhood) %>%
-  				  rownames_to_column("RunNumber") # Doing this assumes snakemake passes each run in order
+# Read in output files
+runs_file <- purrr::map_dfr(bestlhoods, read_delim, delim = "\t") 
+
+runs_summary <- runs_file %>% 
+				select(MaxEstLhood, MaxObsLhood) %>%
+  				mutate(Lhood = MaxObsLhood - MaxEstLhood) %>%
+  				rownames_to_column("RunNumber") # Doing this assumes snakemake passes each run in order
 
 #Pull out best run
 best_run <- runs_summary %>% 
@@ -30,12 +32,32 @@ best_run <- runs_summary %>%
   			select(RunNumber) %>% 
   			pull()
 
+
 # Trim filepath to get model dir
 model_dir <- str_remove(bestlhoods[1], "run[:digit:]+/[:graph:]+")
 
 # Write out runs summary file, named after the best run
 write_delim(runs_summary, paste0(model_dir, "best_run/best_is_", best_run, ".txt"), delim = " ")
 
-# Write log
+## AIC
+# Count the number of parameters in the model
+k <- ncol(runs_file) -2
+
+# Max est lhood
+max_est <- max(runs_summary$MaxEstLhood)
+
+# AIC
+aic <- 2*k-2*(max_est/log10(exp(1)))
+
+# Delta likelihood
+delta_lhood <- runs_summary %>% 
+  			   filter(Lhood == max(Lhood)) %>% 
+  			   select(Lhood) %>% 
+  			   pull()
+write_lines(c(paste("AIC","DeltaL"), paste(aic, delta_lhood)), 
+	paste0(model_dir, "best_run/AIC_", best_run, ".txt"))
+
+
+# Write to log
 sessionInfo()
 
