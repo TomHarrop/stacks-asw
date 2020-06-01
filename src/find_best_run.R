35664c86ef5d2c2ae23d0110baa13a56ad376606
Tom Harrop <twharrop@gmail.com>
2020-06-01 22:10:34 +1200

    too dumb for tidyverse, have to use data.table

--- src/find_best_run.R
+++ src/find_best_run.R
@@ -4,62 +4,65 @@ log <- file(snakemake@log[[1]], open = "wt")
 sink(log, type = "message")
 sink(log, append = TRUE, type = "output")
 
-library(tidyverse)
+library(data.table)
 
 ###########
 # GLOBALS #
 ###########
+
 bestlhoods <-snakemake@input[["bestlhoods"]]
+
+# bestlhoods <- list.files("output/095_fastsimcoal",
+#            pattern = "*.bestlhoods",
+#            recursive = TRUE,
+#            full.names = TRUE)
 
 ########
 # MAIN #
 ########
 
-# Read in output files
-runs_file <- purrr::map_dfr(bestlhoods, read_delim, delim = "\t") 
+names(bestlhoods) <- sapply(strsplit(bestlhoods, "/"), function(x)
+  paste(x[[3]], x[[4]], sep = "."))
 
-runs_summary <- runs_file %>% 
-				select(MaxEstLhood, MaxObsLhood) %>%
-  				mutate(Lhood = MaxObsLhood - MaxEstLhood) %>%
-  				rownames_to_column("RunNumber") # Doing this assumes snakemake passes each run in order
+# parse the names
+runs <- rbindlist(lapply(bestlhoods, fread),
+                  idcol = "filename",
+                  fill = TRUE)
+runs[, c("popset", "pruned", "model", "mig", "run") :=
+       tstrsplit(filename, ".", fixed = TRUE)]
+runs[, filename := NULL]
+runs[, run := gsub(".*run([[:digit:]]{2,3}).*", "\\1", run)]
 
-message(runs_summary)
+# calculate likelihood
+runs[, Lhood := MaxObsLhood - MaxEstLhood]
 
-#Pull out best run
-best_run <- runs_summary %>% 
-  			filter(Lhood == max(Lhood)) %>% 
-  			select(RunNumber) %>% 
-  			pull()
-
-
-# Trim filepath to get model dir
-model_dir <- snakemake@output[["outdir"]]
-
-
-# message(paste("attempting to write file to ", paste0(model_dir, "best_run/best_is_", best_run, ".txt") ))
-# Write out runs summary file, named after the best run
-write_delim(runs_summary, paste0(model_dir, "/best_is_", best_run, ".txt"),
-  delim = " ")
+# count the number of params for each model
+info_cols <- c("MaxEstLhood",
+               "MaxObsLhood",
+               "popset",
+               "pruned",
+               "model", 
+               "mig",
+               "run",
+               "Lhood")
+param_cols <- names(runs)[!names(runs) %in% info_cols]
 
 ## AIC
 # Count the number of parameters in the model
-k <- ncol(runs_file) -2
+runs[, k := sum(!is.na(.SD)),
+     .SDcols = param_cols,
+     by = 1:nrow(runs)]
+runs[, aic := (2 * k) - (2 * (MaxEstLhood/log10(exp(1))))]
+setorder(runs, -Lhood)
 
-# Max est lhood
-max_est <- max(runs_summary$MaxEstLhood)
+# Max est lhood???
+best_run <- runs[is.finite(MaxEstLhood)][which.max(MaxEstLhood)]
+max_est <- runs[, max(MaxEstLhood)]
 
-# AIC
-aic <- 2*k-2*(max_est/log10(exp(1)))
+fwrite(runs,
+       snakemake@output[["summary"]])
+fwrite(best_run,
+       snakemake@output[["bestrun"]])
 
-# Delta likelihood
-delta_lhood <- runs_summary %>% 
-  			   filter(Lhood == max(Lhood)) %>% 
-  			   select(Lhood) %>% 
-  			   pull()
-write_lines(c(paste("AIC","DeltaL"), paste(aic, delta_lhood)), 
-	paste0(model_dir, "/AIC_", best_run, ".txt"))
-
-
-# Write to log
+# log
 sessionInfo()
-
